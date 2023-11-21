fde419ed5e132cebc669120add8b5c79eb54be47
vladf <vlad.fox2002@mail.ru>
2023-11-22 02:15:27 +0300

    ...

--- src/main/java/com/example/bmil_2_att/Bmil2AttApplication.java
+++ src/main/java/com/example/bmil_2_att/Bmil2AttApplication.java
@@ -1,6 +1,7 @@
 package com.example.bmil_2_att;
 
-import com.example.bmil_2_att.model.UserDTO;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,14 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Bmil2AttApplication {
 
     public static void main(String[] args) {
-        System.out.println(
-            new UserDTO(
-                    "User",
-                    "password",
-                    new double[]{0.1 , 0.2, 0.3},
-                    new double[]{0.1 , 0.2, 0.3}
-            )
-        );
+
         SpringApplication.run(Bmil2AttApplication.class, args);
     }
 
