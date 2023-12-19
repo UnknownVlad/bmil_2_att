3d117a47416f2ad1c030216ff54633c868453342
jommka <pretty.pelmeshka@,ail.ru>
2023-12-19 16:19:46 +0300

    фронт регистрации

--- src/main/resources/static/js/time.js
+++ src/main/resources/static/js/time.js
@@ -1,8 +1,39 @@
+// document.getElementById("myForm").addEventListener("submit", function(event){
+//     event.preventDefault();
+//     var password = document.getElementById("password").value;
+//     var time = calculateKeyPressTime(password);
+//     document.getElementById("timeValue").innerHTML = "time";
+    
+// });
+
+
+// function calculateKeyPressTime(password){
+
+//     // Создаем пустой массив для хранения времени нажатий на клавиши
+//     var keyPressTimes = [];
+
+//     // Получаем поле для ввода пароля
+
+//     // При нажатии на клавишу в поле пароля
+//     password.addEventListener('keydown', function(event) {
+//       // Получаем текущее время
+//       var currentTime = new Date().getTime();
+
+//       // Добавляем время нажатия на клавишу в массив
+//       keyPressTimes.push(currentTime);
+//     });
+
+
+//     // var startTime = new Date().getTime();
+//     // var endTime = new Date().getTime();
+//     // var time = endTime - startTime;
+//     return keyPressTimes;
+// }
 
 var keyPressTimes = [];
 $(document).ready(function() {
     var lastKeypressTime;
-
+    
     $("#password").keypress(function() {
       if (lastKeypressTime === undefined) {
         lastKeypressTime = new Date();
@@ -14,6 +45,8 @@ $(document).ready(function() {
       }
     });
   });
+
+
 
   document.getElementById("timeForm").addEventListener("submit", function(event){
     event.preventDefault();
